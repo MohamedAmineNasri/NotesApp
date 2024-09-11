d367575413c383bbc94c7b91f1fdce4337495b95
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-09-11 11:23:01 +0100

    Install and set up react-router-dom for routing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,19 @@
 import Home from "./pages/Home/Home"
-
+import Login from "./pages/Login/Login"
+import SignUp from "./pages/SignUp/SignUp"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
-
+ 
   return (
     <div className="bg-black text-white">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Home/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<SignUp/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- FrontEnd/src/pages/SignUp/SignUp.jsx
+++ FrontEnd/src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,7 @@
 const SignUp = () => {
   return (
     <div>
-      
+      signUp
     </div>
   )
 }
