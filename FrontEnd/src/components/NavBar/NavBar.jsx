ad4db19dde244958dee8d0d19559f092d8702f17
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-09-13 01:13:36 +0100

    Add SearchBar component with input field, search icon, and clear functionality

--- FrontEnd/src/components/NavBar/NavBar.jsx
+++ FrontEnd/src/components/NavBar/NavBar.jsx
@@ -1,21 +1,36 @@
-import { useNavigate } from "react-router-dom"
-import ProfileInfo from "../Cards/ProfileInfo"
+import { useNavigate } from "react-router-dom";
+import ProfileInfo from "../Cards/ProfileInfo";
+import SearchBar from "../SearchBar/SearchBar";
+import { useState } from "react";
 
 const NavBar = () => {
-
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
   const onLogout = () => {
-    navigate('/login')
-  }
+    navigate("/login");
+  };
+  const [searchQuery, SetSearchQuery] = useState("");
+
+  const handleSearch = () => {};
+  const onClearSearch = () => {
+    SetSearchQuery("");
+  };
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-3xl font-medium text-black py-2">Notes</h2>
 
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          SetSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
       <ProfileInfo onLogout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- FrontEnd/src/components/SearchBar/SearchBar.jsx
+++ FrontEnd/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
   return (
-    <div>
-      SearchBar
+    <div className="w-3/5 flex items-center px-4 bg-slate-100 rounded-md ">
+      <input
+        type="text"
+        placeholder="Search Notes"
+        className="w-full text-sm bg-transparent py-[11px] outline-none placehoder: text-center"
+        value={value}
+        onChange={onChange}
+      />
+      {value && (<IoMdClose size={22} className="text-xl text-slate-500 cursor-pointer hover:text-black mr-3" onClick={onClearSearch} />)}
+      <IoSearch size={22} className="text-slate-400 cursor-pointer hover:text-black" onClick={handleSearch}  />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
