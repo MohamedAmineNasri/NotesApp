b0f183ec0dbd5bfdb3a5a7d7bfc971d67254cc6f
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-09-16 18:39:04 +0100

    Implement user registration with full validation and JWT authentication

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const mongoose = require("mongoose");
+
+const jwt = require("jsonwebtoken");
+
+const { authenticateToken } = require("./utilities");
+const User = require("./models/user.model");
 
 mongoose.connect(process.env.CONNECTIONSTRING);
 
@@ -17,6 +22,54 @@ app.use(
 app.get("/", (req, res) => {
   res.json({
     data: "Hello, World",
+  });
+});
+
+//Create Account
+app.use("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "fullName is required" });
+  }
+  if (!email) {
+    return res
+      .status(400)
+      .json({ error: true, message: "fullName is required" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ error: true, message: "password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "User already exist",
+    });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration was successful",
   });
 });
 
--- BackEnd/models/user.model.js
+++ BackEnd/models/user.model.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  fullName: {
-    type: String,
-    email: { type: String },
-    password: { type: String },
-    createdOm: { type: Date, default: new Date().getTime() },
-  },
+  fullName: { type: String },
+  email: { type: String },
+  password: { type: String },
+  createdOm: { type: Date, default: new Date().getTime() },
 });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
