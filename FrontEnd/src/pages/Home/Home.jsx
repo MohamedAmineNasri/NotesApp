3ae34a628d0fe6b60b3a1644f9492483c1e71c7f
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-09-19 21:39:32 +0100

    Integrated Get All Notes API and displayed notes in NoteCard component

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
+import moment from 'moment'
 
 const Home = () => {
   const [openAddEditModal, setOpenAddEditModal] = useState({
@@ -13,46 +14,66 @@ const Home = () => {
     type: "add",
     data: null,
   });
-  
-  const[userInfo, setUserInfo] = useState(null)
 
-  const navigate = useNavigate()
+  const [userInfo, setUserInfo] = useState(null);
+
+  const [allNotes, setAllNotes] = useState([]);
+
+  const navigate = useNavigate();
 
   //Get User Info
   const getUserInfo = async () => {
     try {
-      const response = await axiosInstance.get('/get-user')
-      if(response.data && response.data.user) {
-        setUserInfo(response.data.user)
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
       }
     } catch (error) {
-      if(error.response.status === 401 ) {
-        localStorage.clear()
-        navigate('/login')
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
       }
     }
-  }
+  };
+
+  //Get all notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An unexpexted error occurred. Please try again");
+    }
+  };
 
   useEffect(() => {
-    getUserInfo()
-    return () => {}
-  }, [])
+    getAllNotes();
+    getUserInfo();
+    return () => {};
+  }, []);
 
   return (
     <>
       <NavBar userInfo={userInfo} />
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-          <NoteCard
-            title="Metting on 7th April "
-            date="3ed Apr 2024"
-            content="Meeting on 7th April Meeting on 7th April"
-            tags="#Meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          />
+          {allNotes.map((item, index) => {
+            return(
+              <NoteCard
+              key={item._id}
+              title={item.title}
+              date={moment(item.createdOn).format('DD MMM YYYY')}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+            );
+          })}
         </div>
       </div>
       <button
