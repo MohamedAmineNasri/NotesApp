f8166fed306ffe1c9b4f7f02a1d2de91d834b5b3
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-09-20 13:18:20 +0100

    Implemented Add New Note API integration with modal for note creation

--- FrontEnd/src/components/Input/TagInput.jsx
+++ FrontEnd/src/components/Input/TagInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdClose } from "react-icons/md";
 
 const TagInput = ({ tags, setTags }) => {
   const [inputValue, setInputValue] = useState("");
